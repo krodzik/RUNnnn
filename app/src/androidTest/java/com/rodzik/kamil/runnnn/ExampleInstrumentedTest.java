62520451831ce1c2dba8048cd5afe030b4a1aff1
Kamil Rodzik <kamil.rodzik@yahoo.com>
2016-12-10 19:43:23 +0100

    fix test

--- app/src/androidTest/java/com/rodzik/kamil/runnnn/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/rodzik/kamil/runnnn/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.traincompany.kamil.runnnn", appContext.getPackageName());
+        assertEquals("com.rodzik.kamil.runnnn", appContext.getPackageName());
     }
 }
